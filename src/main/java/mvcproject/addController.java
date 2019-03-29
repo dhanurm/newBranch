6e0d878d61388983431bb4783b4c4af83063d44c
dhanurm <dhanshrimane94@gmail.com>
2019-03-28 17:19:42 -0700

    add controller

--- src/main/java/mvcproject/addController.java
+++ src/main/java/mvcproject/addController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class addController {
 	@RequestMapping("/add")
 	public String add() {
+		//fnsdjfsd
 		System.out.println("inside controller..");
 		return "display.jsp";
 	}
