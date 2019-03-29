73ed8f44fb4cc9fd0f38d3aed77fb23043f6b9b8
dhanurm <dhanshrimane94@gmail.com>
2019-03-28 17:09:52 -0700

    Fisrt Commit

---
+++ src/main/java/mvcproject/addController.java
@@ -0,0 +1,13 @@
+package mvcproject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class addController {
+	@RequestMapping("/add")
+	public String add() {
+		System.out.println("inside controller..");
+		return "display.jsp";
+	}
+}
